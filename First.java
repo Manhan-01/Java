223060d5b25b664c1db2be31f17ad293b33c1833
= <=>
2026-03-28 23:56:14 +0530

    variables

---
+++ First.java
@@ -0,0 +1,9 @@
+class First{
+    public static void main(String args[]){
+
+        int num1 = 5;
+        int num2 = 9;
+        System.out.println(num1 + num2);
+        System.out.println("Hello World");
+    }
+}
