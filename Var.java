bfd5e88fae50c4b273c757bb9e5271896073a3ca
= <=>
2026-03-29 19:52:29 +0530

    Var and Datatypes

---
+++ Var.java
@@ -0,0 +1,15 @@
+public class Var {
+
+    public static void main(String args[]){
+        // byte n = -276;
+        // float num = 5.6f;
+        // System.out.println(n);
+
+
+        int age = 25;
+        double height = 6.1;
+        String name = "Mandy";
+        System.out.println("Name:"+name+",age:"+age+",Height:"+height);
+
+    }
+}
