9914294312cf772cdc8d8916b78c16c8845c7515
Mandy <connectwithmahan@gmail.com>
2026-03-30 23:37:08 +0530

    pre and post incrememt / decrement

--- Practice2_ArithmaticOps.java
+++ Practice2_ArithmaticOps.java
@@ -2,10 +2,22 @@ public class Practice2_ArithmaticOps {
 
     public static void main (String args[]){
         int a =35;
-        int b=75;
-        System.out.println("Addition:"+ (a+b));
-        System.out.println("Substraction:"+(a-b));
-        System.out.println("Multiplication: "+(a*b));
-        System.out.println("Division:"+(a/b));
+        int b=35;
+        // int b=75;
+        // System.out.println("Addition:"+ (a+b));
+        // System.out.println("Substraction:"+(a-b));
+        // System.out.println("Multiplication: "+(a*b));
+        // System.out.println("Division:"+(a/b));
+
+        // System.out.println((a)+(b));
+        System.out.println("before a : "+a+"and b : "+b);
+        System.out.println("after a:"+(a++));
+        System.out.println("after b:"+(++b));
+
+        // a++;
+        // ++b;
+        
+        // System.out.println(a);
+        // System.out.println(b);
     }
 }
