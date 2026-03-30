6419a6db2ffbe76b76aec1b3fd5029a02d446fe2
= <=>
2026-03-30 22:35:19 +0530

    Day 2

---
+++ Practice2.java
@@ -0,0 +1,10 @@
+public class Practice2 {
+
+    public static void main(String args[]){
+        // byte b = 127;
+
+        int a = 256;
+        byte b =(byte)a ;
+        System.out.println(b);
+    }
+}
