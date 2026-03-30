1465ef7a33f81ad567dd1aed3e8e7cbefbd04a4e
Mandy <connectwithmahan@gmail.com>
2026-03-30 23:49:12 +0530

    relational operators

--- Practice2_ArithmaticOps.java
+++ Practice2_ArithmaticOps.java
@@ -3,6 +3,8 @@ public class Practice2_ArithmaticOps {
     public static void main (String args[]){
         int a =35;
         int b=35;
+        char c = 'a';
+        char d = 'd';
         // int b=75;
         // System.out.println("Addition:"+ (a+b));
         // System.out.println("Substraction:"+(a-b));
@@ -10,14 +12,23 @@ public class Practice2_ArithmaticOps {
         // System.out.println("Division:"+(a/b));
 
         // System.out.println((a)+(b));
-        System.out.println("before a : "+a+"and b : "+b);
-        System.out.println("after a:"+(a++));
-        System.out.println("after b:"+(++b));
+        // System.out.println("before a : "+a+"and b : "+b);
+        // System.out.println("after a:"+(a++));
+        // System.out.println("after b:"+(++b));
 
         // a++;
         // ++b;
         
         // System.out.println(a);
         // System.out.println(b);
+
+        // boolean result = a>b;
+        // boolean result = a<b;
+        // boolean result = a==b;
+        // boolean result = a>=b;
+        // System.out.println(result);
+        boolean  result= c >d;
+        // boolean  result= c <d;
+        System.out.println(result);
     }
 }
