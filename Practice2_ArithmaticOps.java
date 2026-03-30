99ee820ac75d9be2409f12eaec97af3a28282b26
Mandy <connectwithmahan@gmail.com>
2026-03-30 23:21:57 +0530

    arithmatic operators

---
+++ Practice2_ArithmaticOps.java
@@ -0,0 +1,11 @@
+public class Practice2_ArithmaticOps {
+
+    public static void main (String args[]){
+        int a =35;
+        int b=75;
+        System.out.println("Addition:"+ (a+b));
+        System.out.println("Substraction:"+(a-b));
+        System.out.println("Multiplication: "+(a*b));
+        System.out.println("Division:"+(a/b));
+    }
+}
