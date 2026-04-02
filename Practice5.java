a710471f035cc937f322ae65a95da2edc9f82af7
Mandy <connectwithmahan@gmail.com>
2026-04-02 22:37:27 +0530

    Calculator

--- Practice5.java
+++ Practice5.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Practice5 {
 
     public static void main(String args[]){
@@ -24,8 +26,42 @@ public class Practice5 {
         //     rev = rev+ name.charAt(i);
         // }
         // System.out.println(rev);
+        // System.out.print(1);
+        // System.out.println(5);
 
 
-        
+
+
+        // Calculator
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of A");
+        double num1 = sc.nextDouble();
+        System.out.println("Enter value of B");
+        double num2 = sc.nextDouble();
+        sc.nextLine();
+        System.out.println("Enter the operation to be performed : ");
+        char operator = (sc.nextLine()).charAt(0);
+        String again = "y";
+        while (again =="y") {
+            if(operator=='+'){
+                System.out.println("Result : "+(num1+num2));
+            }else if(operator == '-'){
+                System.out.println("Result : "+(num1-num2));
+            }else if(operator == '*'){
+                System.out.println("Result : "+(num1*num2));
+            }else if(operator == '/'){
+                if(num2 == 0){
+                    System.out.println("Cannot divide by zero");
+                }else{
+                    System.out.println("Result : "+(num1/num2));
+                }
+            }
+
+            again = "n";
+        }
+        System.out.println("Thank you for using the calculator");
+
+
+
     }
 }
