c1a2ba67e562b63a475c6b752ece398b6dee068d
Mandy <connectwithmahan@gmail.com>
2026-04-02 21:24:38 +0530

    reversing the string

--- Practice5.java
+++ Practice5.java
@@ -3,14 +3,29 @@ public class Practice5 {
     public static void main(String args[]){
 
         // System.out.println(1000/10);
-        int num = 1001999;
-        int count = 0;
+        // int num = 1001999;
+        // int count = 0;
 
-        while (num > 0) {
-            count++;
-            num = num/10;
+        // while (num > 0) {
+        //     count++;
+        //     num = num/10;
             
-        }
-        System.out.println(count);
+        // }
+        // System.out.println(count);
+
+
+
+        // reversin the string
+        // String name = "Mandy";
+        // System.out.println(name.length());
+        // String rev = "";
+        // int num = name.length();
+        // for(int i =num-1; i>=0;i--){
+        //     rev = rev+ name.charAt(i);
+        // }
+        // System.out.println(rev);
+
+
+        
     }
 }
