5b4b77ae13027db0531607ce46d82ddddc4b7a4a
Mandy <connectwithmahan@gmail.com>
2026-04-09 20:53:02 +0530

    importance of .equals()

--- Practice9.java
+++ Practice9.java
@@ -1,12 +1,14 @@
 // Methods
 
+import java.util.Scanner;
+
 class Computer{
     public String playMovie(String movieName){
         String[] moviesList = {"Bahubali","KGF","Pushpa","Dhurandhar","John Wick","Interstellar","With Love","Kantara"};
         int noOfMovies = moviesList.length;
         String resp = "";
         for(int i=0; i<noOfMovies;i++){
-            if(movieName == moviesList[i]){
+            if(movieName.equals(moviesList[i])){
                 resp =  "Now Playing : "+movieName;
                 // Here break is needed because lets say here we are searching for KGF, the for loop iteration starts executing and KGF is 2nd item in the array list
                 // and when KGF is matched with the movie inside the list the resp string will be changed to "Now playing...", and it won't go for else statement.
@@ -30,8 +32,15 @@ class Computer{
 public class Practice9 {
 
     public static void main(String args[]){
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your fav movie name: ");
+        String mName = sc.nextLine();
+        // System.out.println(mName.length());
+
         Computer lenovo = new Computer();
-        String movie = lenovo.playMovie("KGF");
+        String movie = lenovo.playMovie(mName);
         System.out.println(movie);
 
         String music = lenovo.playMusic("Despacito");
