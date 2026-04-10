f6bb71eda943b48f7335d1f42c1e546c7a7ed073
Mandy <connectwithmahan@gmail.com>
2026-04-10 12:11:20 +0530

    Stack vs Heap

---
+++ Practice12.java
@@ -0,0 +1,7 @@
+public class Practice12 {
+
+    public static void main(String args[]){
+        // System.out.println("Hey Mandy!!!, You are doing good, Keep going. This year you will achieve whatever you are dreamed of. Keep Dreaming, Keep Working hard  and bring the dream to reality. This year you will get a good job in FAANG and you will get it in european county. And you will able to travel all over the world and make your parents proud and show them the world as well.");
+    
+    }
+}
