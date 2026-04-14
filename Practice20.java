7200ea6e65c8aed02fca82f46b3a5c05abb18d43
Mandy <connectwithmahan@gmail.com>
2026-04-14 13:11:52 +0530

    Print 2D array, row, column, diagonal numbers

--- Practice20.java
+++ Practice20.java
@@ -5,12 +5,44 @@ public class Practice20 {
         java.util.Random randNum = new java.util.Random();
 
         int nums[][] = new int[3][4];
+
+        // Printing the full array;
         for (int i=0;i<3;i++){
             for (int j=0;j<4;j++){
-                nums[i][j] = randNum.nextInt(100);
+                nums[i][j] = randNum.nextInt(10);
                 System.out.print(nums[i][j]+" ");
             }
             System.out.println();
         } 
-    }
+        System.out.println();
+
+
+        // Print a row in the array;
+
+        for(int i=0;i<4;i++){
+            System.out.print(nums[0][i]+" ");
+        }
+
+        System.out.println();
+        System.out.println();
+
+
+        // Print a column in the array;
+        for(int i=0;i<3;i++){
+            System.out.println(nums[i][0]);
+        }
+
+        // Print diagonal numbers of an array
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                if(nums[i][j] != nums[i][i]){
+                    System.out.print(0+ " ");
+                }else{
+                    System.out.print(nums[i][i]);
+                }
+            }
+            System.out.println();
+        }
+
+        }
 }
