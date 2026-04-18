ee3d34b039e098484036d938c77278edbfab67f2
Mandy <connectwithmahan@gmail.com>
2026-04-18 23:03:15 +0530

    String

---
+++ Practice24.java
@@ -0,0 +1,11 @@
+// String
+
+public class Practice24 {
+
+    public static void main(String args[]){
+        // String name = "Mandy";
+
+        String name = new String("Mandy");
+        System.out.println(name.hashCode());
+    }
+}
