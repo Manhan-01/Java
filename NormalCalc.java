29b68a544f1e5a128da944e7d155f72276221860
Mandy <connectwithmahan@gmail.com>
2026-05-03 22:38:56 +0530

    Creating a Normal calculator

---
+++ NormalCalc.java
@@ -0,0 +1,15 @@
+public class NormalCalc {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int sub(int a, int b){
+        return a-b;
+    }
+    public int mult(int a, int b){
+        return a*b;
+    }
+    public int div(int a, int b){
+        return a/b;
+    }
+}
