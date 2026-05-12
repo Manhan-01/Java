95497a475a971eb537f3d5016922cd2d73c63a1f
Mandy <connectwithmahan@gmail.com>
2026-05-13 00:38:22 +0530

    quiz

---
+++ Practice41.java
@@ -0,0 +1,14 @@
+class A{
+
+
+}
+class B extends A{
+    
+}
+
+public class Practice41 {
+
+    public static void main(String args[]){
+
+    }
+}
---
+++ Practice42.java
@@ -0,0 +1,20 @@
+public class Practice42 {
+
+    public static void main(String args[]){
+        // int arr[] = {1,2,3,4,5};
+        // for(int i=0;i<arr.length;i++){
+        //     System.out.println(arr[i]);
+        // }
+        int arr[][] = {
+            {1,2,3,4,5},
+            {9,8,7,6,5},
+            {9,9,0,1,6}
+        };
+        // for (int i=0;i<)
+        // System.out.println(arr[1][3]);
+
+
+        // below code will print the length of the last row
+        // System.out.println(arr[arr.length-1].length);
+    }
+}
