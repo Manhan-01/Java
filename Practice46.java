6336996da8ce3ccd438dfc1da56a64184c683e5f
Mandy <connectwithmahan@gmail.com>
2026-06-11 11:13:47 +0530

    Reverse the string

---
+++ Practice46.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Practice46 {
+
+    public static void main(String args[]){
+        // System.out.println("Hello World!!!");
+        System.out.println("Code to Reverse the string");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string you want to reverse");
+        String name = sc.nextLine();
+        String rev = "";
+        // System.out.println(name.charAt(2));
+        // System.out.println(name.length());
+        int len = name.length();
+        for(int i = len-1;i>=0;i--){
+            rev = rev+name.charAt(i);
+        }
+        // System.out.println(rev);
+        // System.out.println(rev.toUpperCase());
+
+        System.out.println("Input : "+name.toUpperCase());
+        System.out.println("Output : "+rev.toUpperCase());
+
+        
+    }
+}
