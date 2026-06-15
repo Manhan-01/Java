f0803f1e92ef3896e4deb009be3a5c137f340bc7
Mandy <connectwithmahan@gmail.com>
2026-06-15 23:36:23 +0530

    ENUM if-else

--- Practice48.java
+++ Practice48.java
@@ -15,10 +15,25 @@ public class Practice48 {
         // System.out.println(a);
 
 
-        Status[] a = Status.values();
-        for(Status i:a){
-            System.out.println(i);
+        // Status[] a = Status.values();
+        // for(Status i:a){
+        //     System.out.println(i);
+        // }
+
+
+        Status s = Status.Pending;
+        if(s== Status.Running){
+            System.out.println("All good");
+        }else if(s == Status.Pending){
+            System.out.println("Please Wait");
+        }else if(s == Status.Failed){
+            System.out.println("Try again");
+        }else if(s==Status.Processed){
+            System.out.println("Almost There");
+        }else if(s==Status.Success){
+            System.out.println("Done");
         }
+
         
     }
 }
