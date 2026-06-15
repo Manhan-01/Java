659cde1fc0fef0da0c0f6f05984303c7751d1bb2
Mandy <connectwithmahan@gmail.com>
2026-06-15 23:41:29 +0530

    Switch case in java

--- Practice48.java
+++ Practice48.java
@@ -21,17 +21,41 @@ public class Practice48 {
         // }
 
 
-        Status s = Status.Pending;
-        if(s== Status.Running){
-            System.out.println("All good");
-        }else if(s == Status.Pending){
-            System.out.println("Please Wait");
-        }else if(s == Status.Failed){
-            System.out.println("Try again");
-        }else if(s==Status.Processed){
-            System.out.println("Almost There");
-        }else if(s==Status.Success){
-            System.out.println("Done");
+        // ENUM -if else
+        // Status s = Status.Pending;
+        // if(s== Status.Running){
+        //     System.out.println("All good");
+        // }else if(s == Status.Pending){
+        //     System.out.println("Please Wait");
+        // }else if(s == Status.Failed){
+        //     System.out.println("Try again");
+        // }else if(s==Status.Processed){
+        //     System.out.println("Almost There");
+        // }else if(s==Status.Success){
+        //     System.out.println("Done");
+        // }
+
+
+        // Enum switch
+
+        Status s = Status.Running;
+
+        switch(s){
+            case Running:
+                System.out.println("Everything looking good");
+                break;
+            case Pending:
+                System.out.println("Please wait!, somthing weird going on");
+                break;
+            case Failed:
+                System.out.println("Looking Terrible");
+                break;
+            case Processed:
+                System.out.println("Here you goo");
+                break;
+            case Success:
+                System.out.println("Done");
+                break;
         }
 
         
