3c75fc6dc50229352516e9c76d2ab395e8f27dd5
Mandy <connectwithmahan@gmail.com>
2026-06-25 12:17:17 +0530

    more on lamda expressions

--- Practice55.java
+++ Practice55.java
@@ -3,11 +3,22 @@ interface Animal{
     void bark();
 }
 
+@FunctionalInterface
+interface A{
+    int add(int i, int j);
+}
+
 public class Practice55 {
 
     public static void main(String args[]){
         Animal dog = () -> System.out.println("Dog barks");
         dog.bark();
+
+        A obj = ( i,  j) -> i+j;
+            
+        
+        int result = obj.add(5,4);
+        System.out.println(result);
 
     }
 }
