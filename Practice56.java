e4a68e0418489d6b8015fbd935a59143aed5dca1
Mandy <connectwithmahan@gmail.com>
2026-06-26 13:05:28 +0530

    Exception handling

---
+++ Practice56.java
@@ -0,0 +1,15 @@
+public class Practice56 {
+
+    public static void main(String args[]){
+        int i = 2;
+        int j = 16;
+        try{
+            System.out.println(j/i);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        System.out.println("COmpleted");
+        
+    }
+}
